14f32cee64163f750a8c7e3cad329da69e93f49c
Mariya Nemzorova <nemzorova777@mail.ru>
2023-03-26 01:16:57 +0700

    Solved task 18

--- tasks.py
+++ tasks.py
@@ -32,3 +32,39 @@
 # Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X. 
 # Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. В последующих  
 # строках записаны N целых чисел Ai. Последняя строка содержит число X
+#Задача 18: Требуется найти в массиве A[1..N] самый близкий по величине элемент к заданному числу X.
+#Пользователь в первой строке вводит натуральное число N – количество элементов в массиве. В последующих
+#строках записаны N целых чисел Ai. Последняя строка содержит число X
+
+import sys
+
+input_list = []
+list_len = int(input("Введите количество элементов массива: "))
+for _ in range(list_len):
+    input_list.append(int(input("Введите число: ")))
+print(input_list)
+
+found_num = int(input('Заданное число: '))
+near_num = 0
+
+for i in range(list_len):
+    if input_list[i] == found_num:
+        print('Искомое число присутствует в массиве: ', input_list[i])
+        sys.exit()
+    elif i == 0:
+        if found_num > input_list[i]:
+            min_diff = found_num - input_list[i]
+        else:
+            min_diff = input_list[i] - found_num
+    else:
+        if input_list[i] > found_num:
+            diff = input_list[i] - found_num
+            if min_diff > diff:
+                min_diff = diff
+                near_num = input_list[i]
+        else:
+            diff = found_num - input_list[i]
+            if min_diff > diff:
+                min_diff = diff
+                near_num = input_list[i]
+print('Ближайшее число: ', near_num)
